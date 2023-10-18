d14258c06f4330ed6d5626410086972f29de7097
Daniel Kenan Slinda <sdanielkenan@gmail.com>
2023-10-18 19:44:22 +0200

    final commit

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const session = require('express-session');
 const app = express();
 const port = process.env.PORT || 3000;
 const ejs = require('ejs');
+const nodemailer = require('nodemailer');
+const bodyParser = require('body-parser');
 
 app.set('view engine', 'ejs');
 
@@ -24,24 +26,34 @@ const isAdmin = (req, res, next) => {
     if (user && user.isAdmin) {
         next();
     } else {
-        res.redirect('/signin'); // Redirect to login if not an admin
+        res.redirect('/signin'); 
     }
 };
+
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+      user: 'botauto212@gmail.com',
+      pass: 'cjeifgsiqfivevdx',
+  },
+});
+
 app.use('/adminDashboard', isAdmin);
 
 
 const users = [
-    { name: 'User 1', id: 'user1', email: 'user1@example.com', password: 'user1Password' },
     { name: 'Admin', id: 'admin', email: 'admin@example.com', password: 'adminPassword', isAdmin: true },
 ];
 const applications = [];
 
 const schools = [
-  { name: 'School A', enrolledStudents: 100, pendingApplications: 20 },
-  { name: 'School B', enrolledStudents: 150, pendingApplications: 15 },
+  { name: 'Smithtown High School', enrolledStudents: 0, pendingApplications: 0 },
+  { name: 'Maplewood Academy', enrolledStudents: 0, pendingApplications: 0 },
+  { name: 'Riverside Junior High', enrolledStudents: 0, pendingApplications: 0 },
+  { name: 'Greenwood Elementary School', enrolledStudents: 0, pendingApplications: 0 },
+  { name: 'Pineview Middle School', enrolledStudents: 0, pendingApplications: 0 }
 ];
 
-// Middleware to check if the user is logged in
 const requireLogin = (req, res, next) => {
   if (!req.session.user) {
     res.redirect('/signin');
@@ -50,12 +62,12 @@ const requireLogin = (req, res, next) => {
   }
 };
 
-// Guest View: View available schools
+
 app.get('/schools', (req, res) => {
   res.render('schools', { schools });
 });
 
-// Signup
+
 app.get('/signup', (req, res) => {
   res.render('signup');
 });
@@ -72,50 +84,40 @@ app.post('/signup', (req, res) => {
   }
 });
 
-// Login
+
 app.get('/signin', (req, res) => {
   res.render('login');
 });
 
 app.post('/signin', (req, res) => {
     const { email, password } = req.body;
-    console.log(email,"===",password)
     const user = users.find(u => u.email === email && u.password === password);
 
     if (user) {
-        // Assuming you are using express-session to manage sessions
+   
         req.session.user = user;
         
         if (user.isAdmin) {
-            console.log('ggg')
-            // Redirect to the admin dashboard if the user is an admin
             res.redirect('/adminDashboard');
         } else {
-            // Redirect to the user dashboard for regular users
             res.redirect('/userDashboard');
         }
     } else {
-        // Render the login page with an error message
         res.render('login', { error: 'Invalid Credentials' });
     }
 });
 
 
-
-// User Dashboard
 app.get('/userDashboard', requireLogin, (req, res) => {
   const user = req.session.user;
   res.render('userDashboard', { user });
 });
 
-// Apply to School
 app.post('/apply/:schoolName', requireLogin, (req, res) => {
     const user = req.session.user;
     const schoolName = req.params.schoolName;
     const school = schools.find((s) => s.name === schoolName);
-  
     if (user && school) {
-      // You may want to check if the user has already applied to this school before allowing them to apply again
       const existingApplication = applications.find((app) => app.user.email === user.email && app.school.name === school.name);
       if (existingApplication) {
         res.redirect('/schools');
@@ -128,10 +130,7 @@ app.post('/apply/:schoolName', requireLogin, (req, res) => {
     }
   });
 
-// Track Application
-// Add this route to your server code
 
-// Track Application
 app.get('/trackApplication', requireLogin, (req, res) => {
     const user = req.session.user;
     if (user) {
@@ -142,15 +141,12 @@ app.get('/trackApplication', requireLogin, (req, res) => {
     }
   });
   
-// Admin Dashboard
 app.get('/adminDashboard', isAdmin, (req, res) => {
-    // Assuming you have an array called `applications` that holds all applications
-    const allApplications = applications; // Replace this with your actual data source
+    const allApplications = applications; 
 
     res.render('dashboard', { applications: allApplications });
 });
 
-// Update Application Status
 app.post('/updateStatus/:applicationId', (req, res) => {
   const applicationId = req.params.applicationId;
   const newStatus = req.body.newStatus;
@@ -168,6 +164,11 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
+
+app.get('/forgot-password', (req, res) => {
+  res.render('forgotPassword',{error:null});
+});
+
 app.get('/guest', (req, res) => {
     res.render('guest', { schools });
   });
@@ -176,74 +177,91 @@ app.get('/guest', (req, res) => {
   app.get('/guest.html', (req, res) => {
     res.redirect('/guest');
   });
-  
-  // Redirect for Login Page
+
   app.get('/login.html', (req, res) => {
     res.redirect('/signin');
   });
 
 
-app.get('/application-form', (req, res) => {
-    // Render your application form page
+  app.get('/application-form', requireLogin, (req, res) => {
     const user = req.session.user;
-    res.render('applicationForm', { user: user }    ); // Pass user data to the template
+    if (user && !user.isAdmin) {
+        res.render('applicationForm', { user: user }); 
+    } else {
+        res.send("you are not logged in") 
+    }
 });
 
 app.get('/dashboard', (req, res) => {
-    // Render your user dashboard page
-    res.render('userDashboard', { user: req.user }); // Pass user data to the template
+    res.render('userDashboard', { user: req.user }); 
 });
 
-// Handle application form submission
+app.post('/submit-application', (req, res) => {
+  const { name, email, phone, school, grade } = req.body;
+  const user = req.session.user;
+  const newApplication = { user: user, name: name, email: email, phone: phone, school: school, grade: grade,status: 'Pending',};
+  applications.push(newApplication);
+  const targetSchool = schools.find(s => s.name === school);
+  if (targetSchool) {targetSchool.pendingApplications++;}
+  res.render('applicationConfirmation', { name, school });
+});
+ 
+
+app.get('/submit-application',(req,res)=> res.render("login.ejs"))
 
 app.post('/submit-application', (req, res) => {
     const { name, email, phone, school, grade } = req.body;
 
-    // Assuming you have a user object stored in the session
-    const user = req.session.user;
-
-    // Create a new application object with a default status
-    const newApplication = {
-        user: user,
-        name: name,
-        email: email,
-        phone: phone,
-        school: school,
-        grade: grade,
-        status: 'Pending', // Default status when initially saving the application
-    };
-
-    // Save the application to the database (or push to an array)
-    applications.push(newApplication);
-
-    // Redirect or render a confirmation page
-    res.render('applicationConfirmation', { name, school });
-});
-
-
-// Handle application form submission
-app.post('/submit-application', (req, res) => {
-    const { name, email, phone, school, grade } = req.body;
-
-    // Process the form data (e.g., save to a database)
-
-    // Render the confirmation page with the submitted data
     res.render('applicationConfirmation', { name, school });
 });
 
 app.get('/signout', (req, res) => {
-    // Clear the user session
+    
     req.session.destroy(err => {
         if (err) {
             console.error('Error destroying session:', err);
             res.status(500).send('Internal Server Error');
         } else {
-            // Redirect to the home page or any other desired location after sign-out
+    
             res.redirect('/');
         }
     });
 });
 
+app.post('/forgot-password', (req, res) => {
+  const { email } = req.body;
+  const user = users.find(u => u.email === email);
+  let error = null;
+
+  if (user) {
+      // Send an email with the old password
+      sendOldPasswordEmail(user.email, user.password);
+
+      res.render('oldPasswordSent', { email });
+  } else {
+      error = 'Email not found';
+      res.render('forgotPassword', { error });
+  }
+});
+
+function sendOldPasswordEmail(to, oldPassword) {
+  // Setup email data
+  const mailOptions = {
+    from: 'botauto212@gmail.com',
+    to: to,
+    subject: 'Password Recovery',
+    text: `Your old password is: ${oldPassword}`,
+  };
+
+  // Send the email
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.error('Error sending email:', error);
+    } else {
+      console.log('Email sent:', info.response);
+    }
+  });
+}
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
